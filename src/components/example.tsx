be0992188b18d4c110e6efb6af1e8db9e03f1485
Horacio Daniel Ros <horaciodrs@gmail.com>
2024-08-31 10:04:27 -0300

    check theme and pass it as a parameter to the cedro examples

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,10 +1,14 @@
+import { useColorMode } from "@docusaurus/theme-common";
+
 export default function ({ url, height }) {
+    const { colorMode } = useColorMode();
+    const theme = colorMode === "dark" ? "?theme=cedro-dark" : "?theme=light";
     const baseUrl = "https://cedro-examples.hdrdevs.com.ar/examples/";
 
     return (
         <div style={{ marginBottom: "15px" }}>
             <iframe
-                src={baseUrl + url}
+                src={baseUrl + url + theme}
                 style={{ width: "100%", height: "" + height + "px" }}
             ></iframe>
         </div>
